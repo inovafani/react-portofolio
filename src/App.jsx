6405d989a4e820a27fcf870960337128257bc9e2
inovafani <inoveka@gmail.com>
2025-09-07 20:40:25 +0700

    Finish the navbar section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
 function App() {
   return (
     <>
-      {/* <h1 className="text-4xl text-red-600 font-bold">Hello World</h1> */}
+      <h1 className="text-3xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam ullam
+        laboriosam omnis assumenda tempore animi perspiciatis eveniet dolores,
+        soluta delectus doloremque! Iure molestias vero accusamus officiis, nisi
+        voluptate autem. Ratione reiciendis et porro odio sit ea quidem quod
+        aliquam eaque asperiores error voluptatum animi tenetur ullam aliquid
+        assumenda aspernatur totam laboriosam tempore repudiandae, nihil amet
+        praesentium optio. Illo libero fuga sit dicta dolorum, asperiores
+        perspiciatis ipsam nulla optio nostrum reiciendis porro error minus
+        deleniti voluptatum, doloribus culpa facilis ducimus quos veritatis sed.
+        Neque aliquam recusandae consectetur eius ipsa id excepturi illo nobis?
+        Porro amet in doloremque cumque voluptas quos mollitia obcaecati
+        inventore et maxime, laboriosam nesciunt unde quaerat omnis sint magnam
+        explicabo vero architecto eaque exercitationem ipsam magni accusamus? Ab
+        et quidem recusandae quos, fuga voluptatem rem eveniet sit ea, delectus
+        voluptates qui repellendus perferendis libero atque cumque ipsam illum
+        explicabo maxime obcaecati dolorum neque! Possimus exercitationem
+        excepturi vero omnis itaque autem, libero error aliquid repellat optio
+        illo dicta recusandae qui! Saepe officia omnis optio delectus!
+        Necessitatibus magni dolores incidunt earum harum, facere velit nostrum,
+        veniam aut accusamus pariatur, veritatis deserunt neque quaerat
+        cupiditate suscipit praesentium dolorum itaque? Id temporibus aliquam
+        quidem nihil rem pariatur at hic reiciendis fugit vitae!
+      </h1>
     </>
   );
 }
